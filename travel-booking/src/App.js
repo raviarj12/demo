5aeb99f96adb88f1ebbe0949f03bc74e82a87aa6
raviiraj <rj06an24ce@gmail.com>
2025-03-25 10:25:02 +0530

    add name function

--- travel-booking/src/App.js
+++ travel-booking/src/App.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
+import image from  "./C:\Users\Raviraj\Desktop\travelage\travel-ibk7fgrvtvhs7qzg.webp";
 
 function App() {
+  const myStyle ={
+    backgroundImage: 'img',
+    imageSize: '50xp',
+    imageRehate: 'rehate'
+    
+  }
   // State hooks to manage user input
   const [destination, setDestination] = useState("paris");
   const [passengers, setPassengers] = useState(1);
@@ -32,6 +39,8 @@ function App() {
           id="destination"
           value={destination}
           onChange={(e) => setDestination(e.target.value)}
+
+          // all cuntry booking prisce ret in </option value="......">
         >
           <option value="paris">Paris - $500</option>
           <option value="tokyo">Tokyo - $800</option>
@@ -39,6 +48,27 @@ function App() {
           <option value="london">London - $450</option>
           <option value="Usa">Usa - $560</option>
           <option value="canada">canada - $780</option>
+          <option value="afganistan">afganistan - $670</option>
+          <option value="alabani">alabani - $678</option>
+          <option value="algeria">algeria - $890</option>
+          <option value="andorra">andorra - $60</option>
+          <option value="Antigua and Barbuda">Antigua and Barbuda - $8905</option>
+          <option value="Argentina">Argentina - $345</option>
+          <option value="Armenia">Armenia - $793</option>
+          <option value="Australia">Australia - $645</option>
+          <option value="Austria">Austria - $777</option>
+          <option value="cameroon">cameroon - $467</option>
+          <option value="china">chaina - $890</option>
+          <option value="colimbia">colimbia -$787</option>
+          <option value="cuba">cuba - $878</option>
+          <option value="coritain">coritain - $890</option>
+          <option value="denmark">denmark - $677</option>
+          <option value="fiji">fiji - $898</option>
+          <option value="france">france - $999</option>
+
+          
+
+          
         </select>
 
         <label htmlFor="passengers">Number of Passengers:</label>
@@ -64,3 +94,5 @@ function App() {
 }
 
 export default App;
+
+
